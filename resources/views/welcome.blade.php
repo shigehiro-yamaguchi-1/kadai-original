709018f6c810cb82d4052bf0ff631655407e0fa0
tester <erta.6224@gmail.com>
2017-11-21 10:12:57 +0000

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
                 <tr>
                     <th width="10%" class="text-center">Rank</th>
                     <th width="10%" class="text-center">Score</th>
-                    <th width="60%" class="text-center">title</th>
+                    <th width="50%" class="text-center">title</th>
                     <th width="10%" class="text-center">{!!$high_rate_i!!}</th>
                     <th width="10%" class="text-center">{!!$low_rate_i!!}</th>
                 </tr>
@@ -30,7 +30,7 @@
                 <tr>
                     <td width="10%" class="text-center">{{$ranking->rank}}</td>
                     <td width="10%" class="text-center">{{$ranking->score}}</td>
-                    <td width="60%" class="text-left ellipsis"><img src = "{{ $ranking->profile_image_url }}"> {!! link_to_route('items.item_detail', $ranking->title, ['id' => $ranking->item_id]) !!}</td>
+                    <td width="50%" class="text-left ellipsis"><img src = "{{ $ranking->profile_image_url }}"> {!! link_to_route('items.item_detail', $ranking->title, ['id' => $ranking->item_id]) !!}</td>
                     <td width="10%" class="text-center">{{$ranking->high_rate}}</td>
                     <td width="10%" class="text-center">{{$ranking->low_rate}}</td>
                 </tr>
